eb5caff81cf6e863a26ba341e1a98f376418663c
Andrew-9810 <Andrew-9810@yandex.ru>
2023-04-03 21:23:49 +1000

    add test_post_create

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,11 +1,7 @@
-import shutil
-import tempfile
-from django.contrib.auth import get_user_model
 
-#from deals.forms import TaskCreateForm
+from django.contrib.auth import get_user_model
 from ..models import Post, Group
-from django.conf import settings
-from django.test import Client, TestCase, override_settings
+from django.test import Client, TestCase
 from django.urls import reverse
 from .constants import (
     INDEX_URL_NAME,
@@ -29,40 +25,26 @@ from .constants import (
 
 User = get_user_model()
 
-# Создаем временную папку для медиа-файлов;
-# на момент теста медиа папка будет переопределена
-TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
-
-
-# Для сохранения media-файлов в тестах будет использоваться
-# временная папка TEMP_MEDIA_ROOT, а потом мы ее удалим
-@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostCreateFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Создаем запись в базе данных для проверки сушествующего slug.
+        cls.user = User.objects.create_user(username=AUTHOR_USERNAME)
         cls.group = Group.objects.create(
             title=GROUP_TITLE,
             slug=GROUP_SLUG,
             description=GROUP_DESCRIPTION
         )
+        cls.post = Post.objects.create(
+            text="Andrew",
+            author=cls.user,
+            group=cls.group
+        )
         # Создаем форму, если нужна проверка атрибутов
         #cls.form = TaskCreateForm()
 
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-        # Модуль shutil - библиотека Python с удобными инструментами
-        # для управления файлами и директориями:
-        # создание, удаление, копирование, перемещение, изменение папок и файлов
-        # Метод shutil.rmtree удаляет директорию и всё её содержимое
-        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
-
     def setUp(self):
         # Создаем неавторизованный клиент
-        self.guest_client = Client()
-        self.user = User.objects.create_user(username=AUTHOR_USERNAME)
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
         self.GROUP_ID = self.group.pk
@@ -75,8 +57,7 @@ class PostCreateFormTests(TestCase):
 
         form_data = {
             'text': POST_TEXT,
-            'author': AUTHOR_USERNAME,
-            'group': GROUP_TITLE,
+            'group': self.GROUP_ID,
         }
         # Отправляем POST-запрос
         response = self.authorized_client.post(
@@ -85,14 +66,13 @@ class PostCreateFormTests(TestCase):
             follow=True
         )
         # Проверяем, сработал ли редирект
-        #self.assertRedirects(response, f'/profile/{AUTHOR_USERNAME}/')
+        self.assertRedirects(response, f'/profile/{AUTHOR_USERNAME}/')
         # Проверяем, увеличилось ли число постов
         self.assertEqual(Post.objects.count(), post_count + 1)
         # Проверяем, что создалась запись с заданным слагом
         self.assertTrue(
             Post.objects.filter(
                 text=POST_TEXT,
-                author=AUTHOR_USERNAME,
-                group=GROUP_TITLE,
+                group=self.GROUP_ID,
             ).exists()
         )
